161c7257a8de9594aa616371c10f41c414e55b4c
Tífani Sá <tifanisds@gmail.com>
2026-01-09 17:31:32 -0300

    Adiciona ícone de carregamento

--- src/Components/Helper/Loading.jsx
+++ src/Components/Helper/Loading.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from './Loading.module.css'
+import Carregamento from '../../Assets/carregando.svg?react'
 
 const Loading = () => {
   return (
-    <div>Carregando...</div>
+    <div className={styles.wrapper}>
+      <div className={styles.loading}>
+        <Carregamento />
+      </div>
+    </div>
   )
 }
 
